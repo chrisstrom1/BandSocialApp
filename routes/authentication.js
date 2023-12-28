65cf79a84ed84a239732d6c0091ea4e2c62fdefc
chrisstrom1 <108539898+chrisstrom1@users.noreply.github.com>
2023-12-28 07:43:18 -0600

    Update authentication.js
    ..

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,9 +4,25 @@ let router = express.Router();
 
 let controller = require("../controllers/authentication")
 
+//user login
 router.post("/login", controller.login);
 
+//user register
 router.post("/register", controller.register);
 
-module.exports = router;
+//list specific user
+router.get("/users/:id", userController.showSpecific)
 
+//list all users
+router.get("/users", userController.listAll)
+
+// //create new user
+// router.post("/users", userController.updateExisting)
+
+//update existing user
+router.put("/users", userController.updateExisting)
+
+//delete existing user
+router.delete("/users/:id", userController.deleteUser)
+
+module.exports = router;
